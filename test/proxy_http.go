e5e6859705a5eae47620f034739f135b0065c62f
Dooy <dooy520@qq.com>
2024-03-07 09:11:13 +0800

    proxy

--- test/proxy_http.go
+++ test/proxy_http.go
@@ -8,7 +8,8 @@ import (
 )
 
 func ProxyHandler(w http.ResponseWriter, r *http.Request) {
-	logger.Log.Debug("有请求过来了！: ", r.URL.String())
+	logger.Log.Debug("有请求过来了！")
+	fmt.Println("有请求过来了！!")
 	req, err := http.NewRequest(r.Method, r.URL.String(), r.Body)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
