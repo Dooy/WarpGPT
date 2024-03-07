68a1135a438492f2b39f06f46a57ad20ed431ead
Dooy <dooy520@qq.com>
2024-03-07 10:19:54 +0800

    proxy

--- test/proxy_http_2.go
+++ test/proxy_http_2.go
@@ -3,8 +3,10 @@ package test
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"log"
 	"net"
+	"net/http"
 )
 
 func handleRequest(conn net.Conn) {
@@ -76,6 +78,47 @@ func handleRequest(conn net.Conn) {
 	// io.Copy(targetConn, conn)
 }
 
+func handleConnection(conn net.Conn) {
+
+	defer conn.Close()
+	request, err := http.ReadRequest(bufio.NewReader(conn)) // 读取请求
+	if err != nil {
+		fmt.Printf("Failed to read request: %s", err)
+		return
+	}
+	fmt.Printf("Proxying request to: %s", request.URL.String())
+	client, err := net.Dial("tcp", request.Host) // 建立连接
+	if err != nil {
+		fmt.Printf("Failed to dial server: %s", err)
+		return
+
+	}
+
+	defer client.Close()
+
+	err = request.Write(client) // 替换Host头并发送请求
+	if err != nil {
+		fmt.Printf("Failed to write request: %s", err)
+		return
+	}
+
+	response, err := http.ReadResponse(bufio.NewReader(client), request) // 读取响应
+
+	if err != nil {
+		fmt.Printf("Failed to read response: %s", err)
+		return
+	}
+	defer response.Body.Close()
+	for k, v := range response.Header { // 将响应头写回客户端
+
+		for _, vv := range v {
+			conn.Write([]byte(k + ": " + vv + ""))
+		}
+	}
+	conn.Write([]byte(""))       // 写入响应行与响应头的分隔符
+	io.Copy(conn, response.Body) // 将响应体写回客户端
+}
+
 func StartHttpV2(server string) {
 	listener, err := net.Listen("tcp", server)
 	if err != nil {
@@ -91,6 +134,7 @@ func StartHttpV2(server string) {
 			log.Println("Error accepting connection:", err)
 			continue
 		}
-		go handleRequest(conn)
+		//go handleRequest(conn)
+		go handleConnection(conn)
 	}
 }
